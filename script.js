644b3e5b22c9346d57ab44ee8fa6f5dc54ed3341
Ayush Kumawat <ayushkumawat77889@gmail.com>
2026-03-24 12:42:45 +0530

    4th commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 console.log("Hello, World!");
+const x=()=>{  // Arrow function
+    console.log("This is a function");
+
+}
+function abc(){ // Function declaration
+    console.log("This is another function");
+}
+
+const xm=function abc(){ // Function expression
+    console.log("This is a function expression");
+}
+
+const xd=function (){ // Anonymous function expression
+    console.log("This is an anonymous function expression");
+}
+
+
+const abc={
+    name:"Ayush",
+    age: 25,
+    greet: function(){ // Method in an object
+        console.log("Hello, " + this.name);
+    }
+}
+
+
+
+
+
+
